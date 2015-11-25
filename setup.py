ceae3d59cd43cb0d9c8da3d8393269c2b702ae38
Ivan Ermilov <earthquakesan@gmail.com>
2015-11-25 23:28:26 +0100

    Update setup.py
    Line adds declaration of package namespaces, necessary for successful installation of the package using pip. Fixes https://github.com/ckan/ckanext-pdfview/issues/17

--- setup.py
+++ setup.py
@@ -53,6 +53,9 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    
+    #Fix the KeyError 'ckanext' when installing using pip
+    namespace_packages=['ckanext', 'ckanext.pdfview'],
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
